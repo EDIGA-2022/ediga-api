727ad33b47cee0050086a3f5dc4116b837b3ebcf
Martina Font <marti.font.ocampos@gmail.com>
2022-09-02 19:21:02 -0300

    Required email

--- src/endpoints/auth/register.js
+++ src/endpoints/auth/register.js
@@ -5,16 +5,19 @@ const EdigaUser = db.EdigaUser;
 
 async function register(req, res) {
   const { email, password, name } = req.body
-  if (password.length < 6) {
-    return res.status(400).json({ message: "Password less than 6 characters" })
-  }
+  // Basic validation
   if (!email) {
     return res.status(400).json({message: "Email is required"});
   }
+  if (password.length < 6) {
+    return res.status(400).json({ message: "Password less than 6 characters" })
+  }
+  // Email is already registered
   const dbEmail = await EdigaUser.findOne({ where: { email: email } });
   if (dbEmail) {
     return res.status(400).json({ message: "User already exists" })
   }
+  // Create user with hashed password
   bcrypt.hash(password, 10).then(async (hash) => {
     await EdigaUser.create({
       email,
@@ -33,7 +36,7 @@ async function register(req, res) {
       )
       .catch((error) =>
         res.status(400).json({
-          message: "User not successful created",
+          message: "User unsuccessfully created",
           error: error.message,
         })
       );
