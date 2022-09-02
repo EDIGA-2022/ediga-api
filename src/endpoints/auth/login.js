9dd3f324c1f9c36f9547340d67ecf54892f9142c
Martina Font <marti.font.ocampos@gmail.com>
2022-09-02 19:35:15 -0300

    Add login

--- src/endpoints/auth/login.js
+++ src/endpoints/auth/login.js
@@ -1,17 +1,46 @@
-// const db = require("../db.js");
-// const Photo = db.Photo;
+const db = require("../../db.js");
+const bcrypt = require("bcryptjs");
+
+const EdigaUser = db.EdigaUser;
 
 async function login(req, res) {
-    // console.log("req.params.userId", req.params.userId)
-    // //7a7d8d2b-9a89-4bee-823f-f07b2b50383d
-    // const userId = req.params.userId;
-    // const userPhotos = await Photo.findAll({
-    //     where: {
-    //         userId,
-    //     }
-    // });
-    // console.log("-->", JSON.stringify("userPhotos"))
-    // res.status(200).json(userPhotos);
+
+	const { email, password } = req.body
+	// Check if email and password are provided.
+	if (!email || !password) {
+		return res.status(400).json({
+			message: "Email and password are required",
+		})
+	}
+	try {
+		const user = await EdigaUser.findOne({ where: {email: email} })
+		if (!user) {
+			res.status(400).json({
+				message: "Login unsuccessful",
+				error: "User not found",
+			})
+		} else {
+			// comparing given password with hashed password
+			bcrypt.compare(password, user.password).then(function (result) {
+				result
+					? res.status(200).json({
+						message: "Login successful",
+						user: {
+							edigaUserId: user.edigaUserId,
+							email: user.email,
+							name: user.name
+						},
+					})
+					: res.status(400).json({ message: "Email or password are incorrect. Try again" })
+			})
+		}
+	} catch (error) {
+		res.status(400).json({
+			message: "An error occurred",
+			error: error.message,
+		})
+	}
 }
+
 
 module.exports = login;
