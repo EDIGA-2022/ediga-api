b7e59426136f690239255c54b86072e1264497f4
Martina Font <marti.font.ocampos@gmail.com>
2022-09-02 19:28:06 -0300

    Add curly braces for IDE intelligence

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const Sequelize = require("sequelize");
+const {Sequelize} = require("sequelize");
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
