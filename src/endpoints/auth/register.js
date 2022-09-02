7e3e7816e2d287b648ebb3281a40a4e2cad95883
Martina Font <marti.font.ocampos@gmail.com>
2022-09-02 18:57:45 -0300

    Register endpoint finished

--- src/endpoints/auth/register.js
+++ src/endpoints/auth/register.js
@@ -1,28 +1,40 @@
+const bcrypt = require("bcryptjs");
 const db = require("../../db.js");
 const EdigaUser = db.EdigaUser;
 
+
 async function register(req, res) {
-  const { email, password } = req.body
-	console.log(req.body);
+  const { email, password, name } = req.body
   if (password.length < 6) {
     return res.status(400).json({ message: "Password less than 6 characters" })
   }
-  try {
+  bcrypt.hash(password, 10).then(async (hash) => {
     await EdigaUser.create({
       email,
-      password,
-    }).then(user =>
-      res.status(200).json({
-        message: "User successfully created",
-        user,
-      })
-    )
-  } catch (err) {
-    res.status(401).json({
-      message: "User not successful created",
-      // error: error.mesage,
+      name,
+      password: hash,
     })
-  }
-}
+      .then(user =>
+        res.status(200).json({
+          message: "User successfully created",
+          user: {
+            edigaUserId: user.edigaUserId,
+            email: user.email,
+            name: user.name
+          },
+        })
+      )
+      .catch((error) =>
+        res.status(400).json({
+          message: "User not successful created",
+          error: error.message,
+        })
+      );
+  });
+};
+
+
+
+
 
 module.exports = register;
