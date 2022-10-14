05d11f79b4fbd06a6b690c3776f4b40b3a371b0a
Martina Font <marti.font.ocampos@gmail.com>
2022-10-14 19:23:45 -0300

    Last changes

--- src/endpoints/getUsers.js
+++ src/endpoints/getUsers.js
@@ -3,7 +3,7 @@ const User = db.User;
 
 async function getUsers(req, res) {
     const users = await User.findAll({});
-    console.log("users", JSON.stringify(users))
+    // console.log("users", JSON.stringify(users))
     res.status(200).json(users);
 }
 
