181a429c06e63f331663411b5a56a5f25570042a
Martina Font <marti.font.ocampos@gmail.com>
2022-10-15 22:06:39 -0300

    Fix

--- src/endpoints/exportImages.js
+++ src/endpoints/exportImages.js
@@ -4,12 +4,10 @@ const Photo = db.Photo;
 
 async function exportImages(req, res) {
   const photos = await Photo.findAll({});
-  var first = true;
   // For each photo save the base64 string to a file with the photoId as the filename
   photos.forEach(item => {
     const { photoId, photo } = item;
-    var buffer = Buffer.from(photo, 'utf-8');
-    base64ToImage(buffer, { filePath: "/Users/martinafont/Dev/Fing/EdigaDatos", fileName: photoId }); // success and create a file at temp/img.png or temp/img.jpg.
+    base64ToImage(photo, { filePath: "/Users/martinafont/Dev/Fing/EdigaDatos", fileName: photoId }); // success and create a file at temp/img.png or temp/img.jpg.
   }
   );
   res.status(200).json({
