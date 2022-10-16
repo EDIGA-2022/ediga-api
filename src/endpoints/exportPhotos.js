0f4559ae8a6da2c7c4684a67a9e474147aecde68
Martina Font <marti.font.ocampos@gmail.com>
2022-10-16 16:42:51 -0300

    Endpoint

---
+++ src/endpoints/exportPhotos.js
@@ -0,0 +1,21 @@
+var { imageToBase64, base64ToImage } = require("base64-2-img");
+const db = require("../db.js");
+const Photo = db.Photo;
+
+// Exports all photos stored in database to local folder in system. 
+// Note: PATH to local folder is hardcoded in the function.
+
+async function exportImages(req, res) {
+ const photos = await Photo.findAll({});
+ // For each photo save the base64 string to a file with the photoId as the filename
+ photos.forEach(item => {
+   const { photoId, photo } = item;
+   base64ToImage(photo, { filePath: "/Users/martinafont/Dev/Fing/EdigaDatos", fileName: photoId }); // success and create a file at temp/img.png or temp/img.jpg.
+ }
+ );
+ res.status(200).json({
+   message: "Images exported"
+ });
+}
+ 
+module.exports = exportImages;
