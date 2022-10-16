8598e47ee4f021413e1baed486b97748cc03297b
sofiaalberti <sofialberti2409@gmail.com>
2022-10-15 22:57:24 -0300

    Removed console logs

--- src/endpoints/getUserObservation.js
+++ src/endpoints/getUserObservation.js
@@ -2,10 +2,8 @@ const db = require("../db.js");
 const Observation = db.Observation;
 
 async function getUserObservation(req, res) {
-    console.log("-----", req.params.observationId)
     const observationId = req.params.observationId;
     const userObservation = await Observation.findByPk(observationId);
-    console.log("observationId-->", JSON.stringify(userObservation))
     res.status(200).json(userObservation);
 }
 
