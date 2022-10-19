1fab22e142bcdeff31e61ffe958908d6e3a083c9
Martina Font <marti.font.ocampos@gmail.com>
2022-10-19 14:23:12 -0300

    Auth middleware

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const login = require('./src/endpoints/auth/login');
 const register = require('./src/endpoints/auth/register');
 const passwordReset = require('./src/endpoints/auth/passwordReset');
+const authMiddleware = require('./src/authMiddleware');
 
 //Users
 const getUsers = require('./src/endpoints/getUsers');
@@ -45,6 +46,9 @@ const port = process.env.PORT || 3001;
 app.use(bodyParser.json({ limit: "50mb" }));
 
 app.use(cookieParser());
+
+app.use(authMiddleware);
+
 
 // routes
 // get all users
