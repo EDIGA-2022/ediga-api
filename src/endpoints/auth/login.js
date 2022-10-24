9e755f8173c3c1ea0f09a26cbfbf189e33e0e061
Martina Font <marti.font.ocampos@gmail.com>
2022-10-24 17:22:00 -0300

    Delete logs

--- src/endpoints/auth/login.js
+++ src/endpoints/auth/login.js
@@ -8,9 +8,7 @@ const EdigaUser = db.EdigaUser;
 async function login(req, res) {
 	const { email, password } = req.body
 	// Check if email and password are provided.
-  console.log(req.body);
 	if (!email || !password) {
-    console.log("jsoiaejioa")
 		return res.status(400).json({
 			message: "Email and password are required",
 		})
